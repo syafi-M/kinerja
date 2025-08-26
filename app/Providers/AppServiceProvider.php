c2198e82eebb40bce06781d53a95e409563d359c
syafi-M <syafimq00@gmail.com>
2025-08-26 09:20:15 +0700

    fix: update .gitignore and improve file input handling in editBukti view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::defaultView('vendor.pagination.custom-cuy');
-        
+
         $startTime = Cache::get('app_start_time');
 
         if (!$startTime) {
