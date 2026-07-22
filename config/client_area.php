f02ae9fb4eada200e291aad8acf9788bc2b0c8ca
syafi-M <syafimq00@gmail.com>
2026-07-22 09:39:20 +0700

    add: another city to config

--- config/client_area.php
+++ config/client_area.php
@@ -2,9 +2,9 @@
 
 return [
     'surabaya_raya' => ['surabaya', 'gresik', 'sidoarjo', 'mojokerto', 'lamongan', 'bangkalan'],
-    'madiun_raya' => ['madiun', 'ngawi', 'nganjuk', 'jombang'],
+    'madiun_raya' => ['madiun', 'ngawi', 'nganjuk', 'jombang', 'ponorogo', 'magetan', 'pacitan'],
     'malang_raya' => ['malang', 'batu', 'pasuruan', 'blitar', 'lumajang'],
-    'kediri_raya' => ['kediri', 'tulungagung', 'blitar', 'trenggalek'],
+    'kediri_raya' => ['kediri', 'tulungagung', 'blitar', 'trenggalek', 'nganjuk', 'jombang'],
     'jember_raya' => ['jember', 'bondowoso', 'situbondo', 'banyuwangi', 'lumajang'],
     'probolinggo_raya' => ['probolinggo', 'pasuruan', 'lumajang', 'situbondo'],
     'bojonegoro_raya' => ['bojonegoro', 'tuban', 'lamongan', 'ngawi'],
